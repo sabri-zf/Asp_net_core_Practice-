7f6b96e05d6a77afb630568cd4b2e988b647f9f4
Sabri Zekkour Ferhat <sabriwbe@gmail.com>
2025-09-22 16:06:29 +0100

    tring to understand the basic of ASP.net core architecture start from (client) -> (revers proxy (IIS...)) -> (Kestrel) -> (Asp.Net Core WbeApi) when the server recive the request by Httpcontext object the return response back to client

---
+++ Get_Baisc_foundation_Asp_NETCore/Players.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace build_Project_Using_ASP_NetCore
+{
+
+    sealed public class Player
+    { 
+        public int Id { get; set; }
+        public string Full_Name { get; set; } = null!;
+        public string Team { get; set; } = null!;
+        public byte Age  {get;set; }
+
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            str.AppendLine("---------   Player Card  ------------");
+            str.AppendLine($"Full-Name : {this.Full_Name}");
+            str.AppendLine($"Team      : {this.Team}");
+            str.AppendLine($"Age       : {Age} years old");
+            str.AppendLine("-------------------------------------");
+
+            return str.ToString();
+        }
+
+    }
+
+    public class Players
+    {
+        public static List<Player> ListPlayer => new List<Player>()
+        {
+            new Player(){Id = 1 , Full_Name = "Ryad Mahraz",Team ="Hilale sauid FC",Age = 35},
+            new Player(){Id = 2 , Full_Name = "Mohammed Salah",Team ="Liverpool FC",Age=32},
+            new Player(){Id = 3 , Full_Name = "Pakio Saka",Team ="Arsinal FC",Age = 23},
+        };
+    }
+}

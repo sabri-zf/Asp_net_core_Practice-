b9d38bfbc4b91c651e700c69d640d7322cb329e1
Sabri Zekkour Ferhat <sabriwbe@gmail.com>
2025-09-23 11:41:01 +0100

    applied another way to implement custome middleware class request piplien and got touch about how to apply conditionally middleware using (UseWhen method provide via ASP.NET Core)

---
+++ Overcome_middleware/Middleware/AnotherCustomeMiddleware.cs
@@ -0,0 +1,32 @@
+﻿namespace Overcome_middleware.Middleware
+{
+
+
+    // build middleware class another way to create middleware request pipeline 
+    public class AnotherCustomeMiddleware
+    {
+        // inject it by Asp.net Core via Dependency injection pattren
+        private readonly RequestDelegate _next;
+
+        public AnotherCustomeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync("My name Another middleware class i'm in the Top");
+            await _next(context);
+            await context.Response.WriteAsync("My name Another middleware class i'm in the bottom");
+        }
+    }
+
+
+    public static class AnothercustomeMiddlewareExtension
+    {
+        public static IApplicationBuilder UseAnotherMiddleware (this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<AnotherCustomeMiddleware>();
+        }
+    }
+}

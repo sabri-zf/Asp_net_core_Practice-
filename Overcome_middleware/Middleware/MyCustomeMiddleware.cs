1f31e8a50499afb0932078210d3c5b7641bdef7a
Sabri Zekkour Ferhat <sabriwbe@gmail.com>
2025-09-23 09:44:36 +0100

    create seperate middleware approach by inject it on ServerCollection and call it on program.cs

---
+++ Overcome_middleware/Middleware/MyCustomeMiddleware.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Overcome_middleware.Middleware
+{
+    public class MyCustomeMiddleware : IMiddleware
+    {
+
+        public int x { get; set; }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // before invoke next middleware block of code 
+
+            await context.Response.WriteAsync("\n\nHI welcome to my middleware customesation");
+            await next(context);
+           // after invoke next middleware  block of code 
+        }
+    }
+}
---
+++ Overcome_middleware/Middleware/authoriesationMiddleware.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Overcome_middleware.Middleware
+{
+    public class authoriesationMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            string id = context.Request.Query["id"].ToString();
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                Console.WriteLine("Valid Data ..... !");
+
+                await context.Response.WriteAsync("\n\nWelcome Mr King " + id);   
+            }
+
+           await next(context);
+        }
+    }
+}

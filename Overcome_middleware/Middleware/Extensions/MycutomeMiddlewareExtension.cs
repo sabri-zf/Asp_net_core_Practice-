8b23bdb82f7f3da951561a2993836a2c9fbfe723
Sabri Zekkour Ferhat <sabriwbe@gmail.com>
2025-09-23 10:23:51 +0100

    enhance the code of middleware pipline vthroughout add Extension class to wrapping complicated code

---
+++ Overcome_middleware/Middleware/Extensions/MycutomeMiddlewareExtension.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Overcome_middleware.Middleware.Extensions
+{
+
+    public static class MycutomeMiddlewareExtension
+    {
+        public static IApplicationBuilder SendMessageMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<MyCustomeMiddleware>();
+            return app;
+        }
+    }
+}
